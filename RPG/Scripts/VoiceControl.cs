4c520e666be05b26a1059ce9eaa12cd09d62d2d4
ihartog <26035@ma-web.nl>
2020-02-12 10:16:28 +0100

    voice control script updated

--- RPG/Scripts/VoiceControl.cs
+++ RPG/Scripts/VoiceControl.cs
@@ -9,17 +9,50 @@ public class VoiceControl : Node
     // private int a = 2;
     // private string b = "text";
 
-    private string text;
-
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        SpeechRecognitionEngine recEngine = new SpeechRecognitionEngine();
+        Choices voiceCommands = new Choices();
+        voiceCommands.Add(new string[] { "Block", "Attack", "Parry", "Deflect", "Special" });
+        GrammarBuilder builder = new GrammarBuilder();
+        builder.Append(voiceCommands);
+        Grammar grammar = new Grammar(builder);
 
+        recEngine.LoadGrammarAsync(grammar);
+        recEngine.SetInputToDefaultAudioDevice();
+        recEngine.SpeechRecognized += RecEngine_SpeechRecognized;
+    }
+
+    private void RecEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+    {
+        switch(e.Result.Text)
+        {
+            case "Block":
+
+                break;
+
+            case "Attack":
+
+                break;
+
+            case "Parry":
+
+                break;
+
+            case "Deflect":
+
+                break;
+
+            case "Special":
+
+                break;
+        }
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+  public override void _Process(float delta)
+  {
+       
+  }
 }
