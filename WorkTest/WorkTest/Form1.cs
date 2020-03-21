d8d36c05dd09894087b2d7f120a28d7bb4b0dd24
ihartog <26035@ma-web.nl>
2020-03-21 20:58:18 +0100

    Removed messagebox

--- WorkTest/WorkTest/Form1.cs
+++ WorkTest/WorkTest/Form1.cs
@@ -49,47 +49,41 @@ namespace WorkTest
                 case "Block up":
                     SendKeys.SendWait("{UP}");
                     speechSynth.SpeakAsync("UP");
-                    MessageBox.Show("UP");
                     richTextBox1.Text += "\n" + e.Result.Text;
                     break;
 
                 case "Block right":
                     SendKeys.SendWait("{RIGHT}");
-                    MessageBox.Show("RIGHT");
                     richTextBox1.Text += "\n" + e.Result.Text;
                     break;
 
                 case "Block down":
                     SendKeys.SendWait("{DOWN}");
-                    MessageBox.Show("DOWN");
                     richTextBox1.Text += "\n" + e.Result.Text;
                     break;
 
                 case "Attack":
                   //  SendKeys.SendWait("{ENTER}");
-                    MessageBox.Show("Attacking");
                     richTextBox1.Text += "\n" + e.Result.Text;
                     break;
 
                 case "Parry":
                     SendKeys.SendWait("(p)");
-                    MessageBox.Show("PARIED!");
                     richTextBox1.Text += "\n" + e.Result.Text;
                     break;
 
                 case "Deflect":
+                    SendKeys.SendWait("{DOWN}");
                     SendKeys.SendWait("(e)");
-                    MessageBox.Show("DEFLECTING SINCE 1942");
                     richTextBox1.Text += "\n" + e.Result.Text;
                     break;
 
                 case "Special":
                     SendKeys.SendWait("{TAB}");
-                    MessageBox.Show("USING SPECIAL");
                     richTextBox1.Text += "\n" + e.Result.Text;
                     break;
 
-                case "What do you think of or project?":
+                case "What do you think of our project?":
                     speechSynth.SpeakAsync("It is a very cool project, but i wouldn't expect less from my 2 favorite developers");
                     break;
             }
